b51b2001443b8295136586c5af088f32826dd4aa
fleurhamoen <70647253+fleurhamoen@users.noreply.github.com>
2021-10-25 10:53:44 +0200

    Update vibrtest.ino

--- vibrtest.ino
+++ vibrtest.ino
@@ -1,46 +1,54 @@
-//I just put them in random pins now, but this is how you could define multiple 
+//defining multiple sensors
 int LED_Pin = 13;
 int vibr_Pin1 = 4;
 int vibr_Pin2 = 5;
 int vibr_Pin3 = 6;
 int vibr_Pin4 = 7;
 
-
 void setup(){
+  //set the on-board LED as output
   pinMode(LED_Pin, OUTPUT);
-  pinMode(vibr_Pin1, INPUT); //set vibr_Pin input for measurment
-  //I don't know if what I did here is possible, but they all need to be an INPUT right? 
-  pinMode(vibr_Pin2, INPUT); //set vibr_Pin input for measurment
-  pinMode(vibr_Pin3, INPUT); //set vibr_Pin input for measurment
-  pinMode(vibr_Pin4, INPUT); //set vibr_Pin input for measurment
+  //set vibr_Pin input for measurement
+  pinMode(vibr_Pin1, INPUT); 
+  pinMode(vibr_Pin2, INPUT); 
+  pinMode(vibr_Pin3, INPUT); 
+  pinMode(vibr_Pin4, INPUT); 
 
   Serial.begin(9600); //init serial 9600
  // Serial.println("----------------------Vibration demo------------------------");
 }
 
-//now it only prints the measurement of the first vibration pin, can easily print all if needed by copy-pasting this section
+//this prints the measurement of all the vibration pins, can easily adjust if needed by deleting lines
 void loop(){
   long measurement1 =TP_init1();
   long measurement2 =TP_init2();
   long measurement3 =TP_init3();
   long measurement4 =TP_init4();
   delay(50);
- // Serial.print("measurement = ");
+  Serial.print("measurement = ");
   
-  //for this if-statement, I now made it so that it only prints the first measurement if at least one measurement is above 10000. Needs to be tested.
+  //for this if-statement, it prints all measurements if at least one measurement is above 10000.
   if (measurement1 > 10000 || measurement2 > 10000 || measurement3 > 10000 || measurement4 > 10000){
+    Serial.print("measurement 1: ");
     Serial.println(measurement1);
+    Serial.print("measurement 2: ");
+    Serial.println(measurement2);
+    Serial.print("measurement 3: ");
+    Serial.println(measurement3);
+    Serial.print("measurement 4: ");
+    Serial.println(measurement4);
+
   }
 }
 
-//measurement returns the length of the pulse, so the interval between two vibrations. I don't think this is too important but it shows that it works. Needs to be tested
+//measurement returns the length of the pulse in microseconds or gives up and returns 0 if no complete pulse was received within the timeout. Not important for our design goal, but shows that sensors work
 long TP_init1(){
   delay(10);
   long measurement1=pulseIn (vibr_Pin1, HIGH);  //wait for the pin to get HIGH and returns measurement
   return measurement1;
 }
 
-//made four different functions now, needs to be tested
+//four different function that output vibration measurements
 long TP_init2(){
   delay(10);
   long measurement2=pulseIn (vibr_Pin2, HIGH);  //wait for the pin to get HIGH and returns measurement
